9574b5876cefb01e3269384488c238b601334f4c
Mingrui Jiang <mjray369@gmail.com>
2018-02-10 19:43:05 +0800

    add ML and DL2 classes

--- caption_utility.py
+++ caption_utility.py
@@ -3,13 +3,32 @@ import youtube_dl
 import time
 
 youtube_watch_url = 'https://www.youtube.com/watch?v='
-fast_ai_video_id_array = ['IPBSB1HLNLo',
-                          'JNxcznsrRb8',
-                          '9C06ZPF8Uuc',
-                          'gbceqO8PpBg',
-                          'J99NV9Cr75I',
-                          'sHcLkfRrgoQ',
-                          'H3g26EVADgY']
+fast_ai_video_id_array = [#'IPBSB1HLNLo',
+                          #'JNxcznsrRb8',
+                          #'9C06ZPF8Uuc',
+                          #'gbceqO8PpBg',
+                          #'J99NV9Cr75I',
+                          #'sHcLkfRrgoQ',
+                          #'H3g26EVADgY',
+                          #'cRjPVN3oo4s', # Deep Learning Part Two Lesson 8
+                          #'I-P363wSv0Q',
+                          #'uv0gmrXSXVg',
+                          #'bZmJvmxfH6I',
+                          #'jy1w0mPCHb0',
+                          #'-lx2shfA-5s',
+                          #'1-NYPQw5THU', # Deep Learning Part Two Lesson 14
+                          #'CzdWqFTmn0Y', # Machine Learning 1
+                          #'blyXCk4sgEg',
+                          #'YSFG_W8JxBo',
+                          #'0v93qHDqq_g',
+                          #'3jl2h9hSRvc', # Machine Learning 5
+                          #BFIYUvBRTpE',
+                          #'O5F9vR2CNYI',
+                          #'DzE0eSdy5Hk',
+                          #'PGC0UxakTvM',
+                          #'37sFIak42Sc', # Machine Learning 10
+                          #'XJ_waZlJU8g',
+                          '5_xFdhfUnvQ']
 
 class MyLogger(object):
     def debug(self, msg):
